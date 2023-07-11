011ea0e7441998a6cdafe447d7b63e35e78c4b12
iLovealan1 <124248265+iLovealan1@users.noreply.github.com>
2023-07-11 22:40:40 +0900

    Update DataManager.cs

--- Scripts/SingleTonManager/DataManager.cs
+++ Scripts/SingleTonManager/DataManager.cs
@@ -9,8 +9,6 @@ public partial class DataManager
 
     private DataManager() { }
 
-    public Dictionary<int, TestData> dicTestDatas;
-
     public void LoadAllDatas()
     {
         this.LoadMonsterData();
@@ -26,24 +24,4 @@ public partial class DataManager
         this.LoadEtcItemData();
         this.LoadWeaponData();
     }
-    public void LoadTestData()
-    {
-        TextAsset asset = Resources.Load<TextAsset>("Data/Test_data");
-        string json = asset.text;
-        TestData[] arr = JsonConvert.DeserializeObject<TestData[]>(json);
-        
-
-        this.dicTestDatas = arr.ToDictionary((x) => x.id);
-    }
-
-    public TestData GetDicTestData(int id)
-    {
-        return this.dicTestDatas[id];
-    }
-
-    public List<TestData> GetTestDataList()
-    {
-        return this.dicTestDatas.Values.ToList();
-    }
-
 }
